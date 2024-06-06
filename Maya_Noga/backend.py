11992a61291b5c1254e2a8f916711d0663d50278
Maya Lev <maya.lev.2009@gmail.com>
2024-06-06 19:33:01 +0300

    Altered backend code to only print out top prediction

--- Maya_Noga/backend.py
+++ Maya_Noga/backend.py
@@ -2,19 +2,24 @@ from keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decod
 from PIL import Image
 import numpy as np
 
+detected_objects = []
+
 model = MobileNetV2(weights = "imagenet")
 model.summary()
+
 img = Image.open("goldfish.jpg")
+
 resized_img = img.resize((224, 224))
 img_array = np.array(resized_img)
-img_array
-img_array.shape
+
 input_array = np.expand_dims(img_array,axis=0)
-input_array.shape
+
 input_data = preprocess_input(input_array)
-input_data
+
 predictions = model.predict(input_data)
-predictions
-print(np.argmin(predictions, axis=1))
-for name, desc, score in decode_predictions(predictions, top=3)[0]:
-  print("{} - probability of ({:.2f}%)".format(desc.replace("_"," ").capitalize(), score*100))
+
+decoded_predictions = decode_predictions(predictions, top=1)
+top_prediction = decoded_predictions[0][0][1]
+
+detected_objects.append(top_prediction.replace('_', ' ').capitalize())
+print(detected_objects[0])
