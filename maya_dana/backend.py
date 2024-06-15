7328e511b14b4021edd51ca964685cb37739b655
DanaNoudel <dana.pavnud@gmail.com>
2024-06-15 21:12:40 +0300

    'New Dog' button work (the API part)

--- maya_dana/backend.py
+++ maya_dana/backend.py
@@ -1,19 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
 from keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
 from PIL import Image
 import numpy as np
+from io import BytesIO
+import requests
 
-model = MobileNetV2(weights = "imagenet")
-model.summary()
-img = Image.open("shih-tzu.jpg")
-resized_img = img.resize((224, 224))
-img_array = np.array(resized_img)
+app = Flask(__name__)
+model = MobileNetV2(weights='imagenet')
 
-input_array = np.expand_dims((img_array), axis=0)
-input_array.shape
-input_data = preprocess_input(input_array)
+def image(img):
+    resized_img = img.resize((224, 224))
+    img_array = np.array(resized_img)
+    input_array = np.expand_dims(img_array, axis=0)
+    input_data = preprocess_input(input_array)
+    return input_data
 
-predictions = model.predict(input_data)
-predictions.shape
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-for name, desc, score in decode_predictions(predictions, top=1)[0]:
-  print(f"{desc}")
+@app.route('/new_dog', methods=['POST'])
+def new_dog():
+    response = requests.get("https://api.thedogapi.com/v1/images/search").json()[0]["url"]
+    img_response = requests.get(response)
+    img = Image.open(BytesIO(img_response.content))
+
+    input_data = image(img)
+    predictions = model.predict(input_data)
+    decoded_predictions = decode_predictions(predictions, top=1)[0]
+    result = decoded_predictions[0][1]
+
+    return render_template('index.html', dog_image=response, breed=result)
+
+if __name__ == '__main__':
+    app.run()
